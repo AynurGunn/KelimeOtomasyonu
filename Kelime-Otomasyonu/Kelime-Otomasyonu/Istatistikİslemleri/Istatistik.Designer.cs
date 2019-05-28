8f8edc67b6196de319373985b0a79be437a112b4
AynurGunn <aynur.gunn@hotmail.com>
2019-05-28 09:51:30 +0300

    -

---
+++ Kelime-Otomasyonu/Kelime-Otomasyonu/Istatistikİslemleri/Istatistik.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Kelime_Otomasyonu.Istatistikİslemleri
+{
+    partial class Istatistik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cKelime = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.cKelime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cKelime
+            // 
+            this.cKelime.BackColor = System.Drawing.Color.Wheat;
+            chartArea1.CursorX.AxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            chartArea1.CursorX.Interval = 0D;
+            chartArea1.CursorX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Months;
+            chartArea1.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Months;
+            chartArea1.CursorX.LineWidth = 0;
+            chartArea1.Name = "ChartArea1";
+            this.cKelime.ChartAreas.Add(chartArea1);
+            this.cKelime.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.cKelime.Legends.Add(legend1);
+            this.cKelime.Location = new System.Drawing.Point(0, 0);
+            this.cKelime.Name = "cKelime";
+            this.cKelime.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Kelime";
+            this.cKelime.Series.Add(series1);
+            this.cKelime.Size = new System.Drawing.Size(724, 482);
+            this.cKelime.TabIndex = 0;
+            this.cKelime.Text = "chart1";
+            // 
+            // Istatistik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 482);
+            this.Controls.Add(this.cKelime);
+            this.Name = "Istatistik";
+            this.Text = "Istatistik";
+            this.Load += new System.EventHandler(this.Istatistik_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cKelime)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart cKelime;
+    }
+}
---
+++ Kelime-Otomasyonu/Kelime-Otomasyonu/Istatistikİslemleri/Istatistik.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Data.SqlClient;
+
+namespace Kelime_Otomasyonu.Istatistikİslemleri
+{
+    public partial class Istatistik : DevExpress.XtraEditors.XtraForm
+
+
+    {
+        public Istatistik()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglinti = new SqlConnection("Data Source=BEYZATAS;Initial Catalog=KelimeOtomasyonu;Integrated Security=True");
+
+        private void Istatistik_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(500, 140);
+
+            baglinti.Open();
+            SqlCommand komutg = new SqlCommand("Select ID,SUM(BilinenKelimeSayisi) from tbl_Istatistik group by ID", baglinti);
+            SqlDataReader read = komutg.ExecuteReader();
+
+            while (read.Read())
+            {
+
+                cKelime.Series["Kelime"].Points.AddXY(int.Parse(read[0].ToString()), int.Parse(read[1].ToString()));
+            }
+            baglinti.Close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //baglinti.Open();
+            //SqlCommand komutg = new SqlCommand("Select BilinenKelimeSayisi from tbl_Istatistik BilinenKelimeSayisi", baglinti);
+            //SqlDataReader read = komutg.ExecuteReader();
+
+            //while(read.Read())
+            //{
+
+            //    cKelime.Series["Kelime"].Points.AddXY("OCAK  ŞUBAT  MART  NİSAN  MAYIS  HAZİRAN  TEMMUZ  AĞUSTOS  EYLÜL  EKİM  KASIM  ARALIK", read[0]);
+            //}
+            //baglinti.Close();
+
+        }
+    }
+}
